5e257ce996c7160f6edd98fb03b16e61c322c8c1
Rajpeer <31372812+RajCollection@users.noreply.github.com>
2017-08-30 15:46:41 +0530

    app.js file created

---
+++ app.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var path = require('path');
+var smtpTransport = require('nodemailer-smtp-transport');
+var config = JSON.parse(fs.readFileSync("config.json"));
+var nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+   service: 'gmail',
+   secure: false,
+   port: 25,
+   auth:{
+       user: 'rajpeer141@gmail.com', //your mail id
+       pass: 'xxxxxxxxxxx'  //password of your account
+   },
+   tls: {
+       rejectUnauthorized: false
+   }
+
+});
+
+let HelperOptions = {
+   from: '"raj peer" <rajpeer141@gmail.com>', //your mail id
+   to: 'rajmtech7@gmail.com',  // to whom you are sending
+   subject: 'form nodejs',   
+   text: "hiiiii"
+
+};
+transporter.sendMail(HelperOptions, (error, info)=>{
+   if (error) {
+       return console.log(error);
+   }
+   console.log("email sent");
+   console.log(info);
+});
+
+         
+
